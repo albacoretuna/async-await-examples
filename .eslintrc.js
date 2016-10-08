c141ba0583e2c49bf5b6d34c617b6acf913f9950
Omid Hezaveh <a@gik.fi>
2016-10-08 06:16:58 +0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "env": {
+        "es7": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
---
+++ index.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const request = require('request-promise');
+
+const headers = {
+    'User-Agent': 'scottwrobinson'
+};
+
+const repos = [
+    'scottwrobinson/camo',
+    'facebook/react',
+    'scottwrobinson/twentyjs',
+    'moment/moment',
+    'nodejs/node',
+    'lodash/lodash'
+];
+
+const issueTitles = [];
+
+async function main() {
+
+    let reqs = repos.map(async function(r) {
+        let options = { url: 'https://api.github.com/repos/' + r, headers: headers };
+        let body = await request.get(options);
+        let json = JSON.parse(body);
+        if (json.has_issues) {
+            let issuesOptions = { url: 'https://api.github.com/repos/' + r + '/issues', headers: headers };
+            let ibody = await request.get(issuesOptions);
+            let issuesJson = JSON.parse(ibody);
+
+            if (issuesJson[0]) {
+                issueTitles.push(issuesJson[0].title);
+                console.log(JSON.stringify('issueTitles inside main', issueTitles));
+            }
+        }
+    });
+
+    await Promise.all(reqs);
+}
+
+main();
+
+console.log(JSON.stringify(issueTitles));
